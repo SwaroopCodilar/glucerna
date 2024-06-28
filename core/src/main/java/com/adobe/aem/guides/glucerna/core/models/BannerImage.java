c915f0a608184ea5375c054bbd129440253ab41e
nagendra <snagendrap7@gmail.com>
2024-06-28 18:11:26 +0530

    Created banner components code

---
+++ core/src/main/java/com/adobe/aem/guides/glucerna/core/models/BannerImage.java
@@ -0,0 +1,39 @@
+package com.adobe.aem.guides.glucerna.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class BannerImage {
+
+    @ValueMapValue
+    private  String desktopRefernce;
+
+    @ValueMapValue
+    private String desktoptitle;
+
+    @ValueMapValue
+    private String mobileReference;
+
+    @ValueMapValue
+    private String mobiletitle;
+
+    public String getDesktopRefernce() {
+        return desktopRefernce;
+    }
+
+    public String getDesktoptitle() {
+        return desktoptitle;
+    }
+
+    public String getMobileReference() {
+        return mobileReference;
+    }
+
+    public String getMobiletitle() {
+        return mobiletitle;
+    }
+
+}
