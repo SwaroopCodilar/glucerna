b75e88be40fca622a56dcf70031f345e63451c55
SwaroopCodilar <swaroop.k@codilar.com>
2024-06-28 17:17:16 +0530

    Update navigation.js

--- ui.frontend/src/main/webpack/components/navigation/navigation.js
+++ ui.frontend/src/main/webpack/components/navigation/navigation.js
@@ -3,18 +3,17 @@ ham.addEventListener('click', () => {
     ham.parentElement.classList.toggle('open');
 });
 document.addEventListener('DOMContentLoaded', function() {
-    const modal = document.getElementById("navbar-close");
-    const closeBtn = document.getElementById("hello-id");
+
+    //const closeBtn = document.getElementById("hello-id img");
     const hamburgerImage = document.querySelector('.hamburger-bar-image');
     const crossImage = document.querySelector('.hamburger-bar-cross');
+
     hamburgerImage.addEventListener('click', function() {
         hamburgerImage.style.display = 'none';
         crossImage.style.display = 'block';
-        modal.style.display = "block";
     });
     crossImage.addEventListener('click', function() {
         crossImage.style.display = 'none';
         hamburgerImage.style.display = 'block';
-        modal.style.display = "none";
     });
 });
