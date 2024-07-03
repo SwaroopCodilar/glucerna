14a874ff7b7a6d51c0538270b7b7d36e5371a822
Naveennn2358 <naveen.n@codilar.com>
2024-07-03 16:20:13 +0530

    latest commit

--- ui.frontend/src/main/webpack/components/videocomponent/videocomponent.js
+++ ui.frontend/src/main/webpack/components/videocomponent/videocomponent.js
@@ -1,21 +1,25 @@
-   document.getElementById('playButton').addEventListener('click', function () {
-        var video = document.getElementById('myVideo');
-        video.setAttribute('controls', 'controls');
-        video.play();
-        this.style.display = 'none';
-    });
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('play-button')) {
+        var container = event.target.closest('.video-container');
+        var video = container.querySelector('video');
+        if (video) {
+            video.setAttribute('controls', 'controls');
+            video.play();
+            event.target.style.display = 'none';
 
-    document.getElementById('myVideo').addEventListener('play', function () {
-        document.getElementById('playButton').style.display = 'none';
-    });
+            video.addEventListener('play', function () {
+                event.target.style.display = 'none';
+            });
 
-    document.getElementById('myVideo').addEventListener('pause', function () {
-        var video = document.getElementById('myVideo');
-        if(video.currentTime === 0 || video.currentTime === video.duration) {
-            document.getElementById('playButton').style.display = 'block';
+            video.addEventListener('pause', function () {
+                if (video.currentTime === 0 || video.currentTime === video.duration) {
+                    event.target.style.display = 'block';
+                }
+            });
+
+            video.addEventListener('ended', function () {
+                event.target.style.display = 'block';
+            });
         }
-    });
-
-    document.getElementById('myVideo').addEventListener('ended', function () {
-        document.getElementById('playButton').style.display = 'block';
-    });
+    }
+});
