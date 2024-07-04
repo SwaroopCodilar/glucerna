18ad5bba71b7f7c569fdf63a09251e2411797411
Naveennn2358 <naveen.n@codilar.com>
2024-07-04 11:44:29 +0530

    Converted ES5 to ES6

--- ui.frontend/src/main/webpack/components/videocomponent/videocomponent.js
+++ ui.frontend/src/main/webpack/components/videocomponent/videocomponent.js
@@ -1,23 +1,24 @@
-document.addEventListener('click', function (event) {
+document.addEventListener('click', (event) => {
     if (event.target.classList.contains('play-button')) {
-        var container = event.target.closest('.video-container');
-        var video = container.querySelector('video');
+        const container = event.target.closest('.video-container');
+        const video = container.querySelector('video');
+
         if (video) {
             video.setAttribute('controls', 'controls');
             video.play();
             event.target.style.display = 'none';
 
-            video.addEventListener('play', function () {
+            video.addEventListener('play', () => {
                 event.target.style.display = 'none';
             });
 
-            video.addEventListener('pause', function () {
+            video.addEventListener('pause', () => {
                 if (video.currentTime === 0 || video.currentTime === video.duration) {
                     event.target.style.display = 'block';
                 }
             });
 
-            video.addEventListener('ended', function () {
+            video.addEventListener('ended', () => {
                 event.target.style.display = 'block';
             });
         }
